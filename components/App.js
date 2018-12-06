a948d8b6e3b583013ffc9e99264c564395024f71
impersin <closer124@gmail.com>
2018-12-06 14:38:04 -0800

    Created initial splash screen

--- components/App.js
+++ components/App.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#4F6D7A'
   },
   welcome: {
     fontSize: 20,
